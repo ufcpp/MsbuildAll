b211791ad406ef01c82024fb5855c8c5d97e9829
niwanaga <iwanaga@orange-cube.jp>
2015-06-25 13:06:03 +0900

    最低ラインのファイル追加

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using FluentArithmetic;
+using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 1
+                .Add(2)
+                .Mul(3)
+                .Sub(1)
+                .Div(4);
+
+            Console.WriteLine(x);
+        }
+    }
+}
