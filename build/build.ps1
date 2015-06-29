402e0116e5ed2a1404f85547485834e92472e844
niwanaga <iwanaga@orange-cube.jp>
2015-06-29 10:19:44 +0900

    .gitignore から build/ 削除

---
+++ build/build.ps1
@@ -0,0 +1,43 @@
+﻿$Script:msbuild = .\Get-VsPath.ps1
+
+function build
+{
+    process
+    {
+        $Local:sln = $_
+
+        ./NuGet.exe restore $Local:sln
+
+        . $Script:msbuild $Local:sln /t:Build
+    }
+}
+
+function build-dependecy
+{
+    process
+    {
+        $Local:repo = $_
+
+        try
+        {
+            pushd $('..\..\' + $Local:repo + '\build')
+            & .\build.ps1
+        }
+        finally
+        {
+            popd
+        }
+    }
+}
+
+cat ./solutions.txt | build
+
+if (Test-Path dependency.txt)
+{
+    cat ./dependency.txt | build-dependecy
+}
+
+if (Test-Path postbuild.ps1)
+{
+    . ./postbuild.ps1
+}
